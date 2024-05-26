ed35c0a507acb3f354533473ee2662ce7b9d3634
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-05-26 15:45:34 -0700

    feat(scene): create a loader node for background loading

---
+++ scene/loader.gd
@@ -0,0 +1,151 @@
+##
+## std/scene/loader.gd
+##
+## Loader is a node which can load packed scenes using background threads.
+##
+
+extends Node
+
+# -- SIGNALS ------------------------------------------------------------------------- #
+
+signal scene_loaded(path: String, packed_scene: PackedScene)
+
+# -- DEPENDENCIES -------------------------------------------------------------------- #
+
+# -- DEFINITIONS --------------------------------------------------------------------- #
+
+const _TYPE_HINT_PACKED_SCENE := "PackedScene"
+
+
+class Result:
+	extends RefCounted
+
+	signal done
+
+	var _path: String
+	var _packed_scene: PackedScene
+	var _status: ResourceLoader.ThreadLoadStatus = (
+		ResourceLoader.THREAD_LOAD_IN_PROGRESS
+	)
+
+	static func new_with_path(path: String) -> Result:
+		assert(path != "", "missing argument: path")
+
+		var result := Result.new()
+		result._path = path
+
+		return result
+
+	func get_path() -> String:
+		return _path
+
+	func get_status() -> int:
+		return _status
+
+	func get_packed_scene() -> PackedScene:
+		return null
+
+	func _set_packed_scene(packed_scene: PackedScene) -> void:
+		assert(packed_scene != null, "missing input: packed_scene")
+		assert(_packed_scene, "result already set")
+
+		_packed_scene = packed_scene
+		done.emit()
+
+	func _set_status(status: ResourceLoader.ThreadLoadStatus) -> void:
+		_status = status
+
+
+# -- CONFIGURATION ------------------------------------------------------------------- #
+
+# -- INITIALIZATION ------------------------------------------------------------------ #
+
+var _to_load: Dictionary = {}
+
+# -- PUBLIC METHODS ------------------------------------------------------------------ #
+
+
+func load_scene(path: String, use_sub_threads: bool = true) -> int:
+	assert(path.begins_with("res://"), "expected path to be absolute")
+	assert(
+		path.ends_with(".tscn") or path.ends_with(".scn"),
+		"expected path to be a packed scene"
+	)
+
+	var is_loading: bool = path in _to_load
+	if not is_loading:
+		var err := (
+			ResourceLoader
+			. load_threaded_request(
+				path,
+				_TYPE_HINT_PACKED_SCENE,
+				use_sub_threads,
+			)
+		)
+
+		if err != OK:
+			return err
+
+		var status := ResourceLoader.load_threaded_get_status(path)
+		if (
+			status == ResourceLoader.THREAD_LOAD_INVALID_RESOURCE
+			or status == ResourceLoader.THREAD_LOAD_FAILED
+		):
+			return FAILED
+
+	_to_load[path] = Result.new_with_path(path)
+
+	return OK
+
+
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
+
+
+func _process(_delta: float):
+	var completed := PackedStringArray()
+
+	for path in _to_load:
+		var result: Result = _to_load[path]
+		assert(result != null, "invalid state; missing result")
+
+		var status := ResourceLoader.load_threaded_get_status(path)
+		assert(
+			(
+				status != ResourceLoader.THREAD_LOAD_INVALID_RESOURCE
+				and status != ResourceLoader.THREAD_LOAD_FAILED
+			),
+			"failed to load resource"
+		)
+
+		result._set_status(status)
+
+		if status == ResourceLoader.THREAD_LOAD_LOADED:
+			completed.append(path)
+
+	for path in completed:
+		var result: Result = _to_load[path]
+		assert(result != null, "invalid state; missing result")
+
+		var packed_scene: PackedScene = ResourceLoader.load_threaded_get(path)
+		assert(packed_scene != null, "loaded scene was unexpectedly null")
+
+		result._set_packed_scene(packed_scene)
+		scene_loaded.emit(path, packed_scene)
+
+		_to_load.erase(path)
+
+
+# -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+# -- PRIVATE METHODS ----------------------------------------------------------------- #
+
+
+func _publish_loaded_scene(path: String):
+	var packed_scene: PackedScene = ResourceLoader.load_threaded_get(path)
+	assert(packed_scene != null, "loaded scene was unexpectedly null")
+
+	scene_loaded.emit(path, packed_scene)
+
+# -- SIGNAL HANDLERS ----------------------------------------------------------------- #
+
+# -- SETTERS/GETTERS ----------------------------------------------------------------- #
