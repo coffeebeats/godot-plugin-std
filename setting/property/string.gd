78ec8de1e618f506a70546f58ce18d794a3c5736
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-11-01 14:09:00 -0700

    feat(setting): create resources which uniquely identify a setting property

---
+++ setting/property/string.gd
@@ -0,0 +1,26 @@
+##
+## std/setting/property/string.gd
+##
+## SettingsStringProperty is a handle that identifies a single 'String' property within
+## a settings scope (i.e. repository). It can also be used to modify a provided 'Config'
+## instance.
+##
+
+class_name SettingsStringProperty
+extends "property.gd"
+
+# -- CONFIGURATION ------------------------------------------------------------------- #
+
+## default is the value that will be returned from 'Config' reads if the property
+## doesn't have a value defined.
+@export var default: String
+
+# -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+
+func _get_value_on_config(config: Config) -> Variant:
+	return config.get_string(category, name, default)
+
+
+func _set_value_on_config(config: Config, value: String) -> bool:
+	return config.set_string(category, name, value)
