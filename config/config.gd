7d98b4cc4ecfdfec5ed9cfc8e0ad213b206ec6df
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-11-05 11:51:04 -0800

    feat(config): add support for storing `Vector2` values

--- config/config.gd
+++ config/config.gd
@@ -13,7 +13,7 @@ extends RefCounted
 
 # -- SIGNALS ------------------------------------------------------------------------- #
 
-## changed is emitted whenever the value associated with a 'key' in a 'category' is
+## changed is emitted whenever the value associated with a `key` in a `category` is
 ## changed to a new value.
 signal changed(category: StringName, key: StringName)
 
@@ -24,68 +24,84 @@ var _data: Dictionary = {}
 # -- PUBLIC METHODS ------------------------------------------------------------------ #
 
 
-## erase clears the value associated with the 'key' in 'category.
+## erase clears the value associated with the `key` in `category`.
 func erase(category: StringName, key: StringName) -> bool:
 	return _delete_key(category, key)
 
 
-## get_float retrieves the value associated with 'key' in 'category' if one is set and
+## get_float retrieves the value associated with `key` in `category` if one is set and
 ## it's type is a 'float'. If no value is associated then 'default' is returned.
 func get_float(category: StringName, key: StringName, default: float) -> float:
 	var value: Variant = _get_variant(category, key)
 	return value if value is float else default
 
 
-## get_int retrieves the value associated with 'key' in 'category' if one is set and
+## get_int retrieves the value associated with `key` in `category` if one is set and
 ## it's type is a 'int'. If no value is associated then 'default' is returned.
 func get_int(category: StringName, key: StringName, default: int) -> int:
 	var value: Variant = _get_variant(category, key)
 	return value if value is int else default
 
 
-## get_string retrieves the value associated with 'key' in 'category' if one is set and
-## it's type is a 'String'. If no value is associated then 'default' is returned.
+## get_string retrieves the value associated with `key` in `category` if one is set and
+## it's type is a `String`. If no value is associated then 'default' is returned.
 func get_string(category: StringName, key: StringName, default: String) -> String:
 	var value: Variant = _get_variant(category, key)
 	return value if value is String else default
 
+## get_vector2 retrieves the value associated with `key` in `category` if one is set and
+## it's type is a 'vector2'. If no value is associated then 'default' is returned.
+func get_vector2(category: StringName, key: StringName, default: Vector2) -> Vector2:
+	var value: Variant = _get_variant(category, key)
+	return value if value is Vector2 else default
 
-## has_float returns whether there is a 'float'-typed value associated with 'key' in
-## 'category'.
+## has_float returns whether there is a 'float'-typed value associated with `key` in
+## `category`.
 func has_float(category: StringName, key: StringName) -> bool:
 	var value: Variant = _get_variant(category, key)
 	return value is float
 
 
-## has_int returns whether there is a 'int'-typed value associated with 'key' in
-## 'category'.
+## has_int returns whether there is a 'int'-typed value associated with `key` in
+## `category`.
 func has_int(category: StringName, key: StringName) -> bool:
 	var value: Variant = _get_variant(category, key)
 	return value is int
 
 
-## has_string returns whether there is a 'String'-typed value associated with 'key' in
-## 'category'.
+## has_string returns whether there is a `String`-typed value associated with `key` in
+## `category`.
 func has_string(category: StringName, key: StringName) -> bool:
 	var value: Variant = _get_variant(category, key)
 	return value is String
 
+## has_vector2 returns whether there is a `Vector2`-typed value associated with `key` in
+## `category`.
+func has_vector2(category: StringName, key: StringName) -> bool:
+	var value: Variant = _get_variant(category, key)
+	return value is Vector2
 
-## set_float updates the value associated with 'key' in 'category' and returns whether
+
+## set_float updates the value associated with `key` in `category` and returns whether
 ## the value was changed.
 func set_float(category: StringName, key: StringName, value: float) -> bool:
 	return _set_variant(category, key, value)
 
 
-## set_int updates the value associated with 'key' in 'category' and returns whether the
+## set_int updates the value associated with `key` in `category` and returns whether the
 ## value was changed.
 func set_int(category: StringName, key: StringName, value: int) -> bool:
 	return _set_variant(category, key, value)
 
 
-## set_string updates the value associated with 'key' in 'category' and returns whether
+## set_string updates the value associated with `key` in `category` and returns whether
 ## the value was changed.
 func set_string(category: StringName, key: StringName, value: String) -> bool:
+	return _set_variant(category, key, value)
+
+## set_vector2 updates the value associated with `key` in `category` and returns whether
+## the value was changed.
+func set_vector2(category: StringName, key: StringName, value: Vector2) -> bool:
 	return _set_variant(category, key, value)
 
 
