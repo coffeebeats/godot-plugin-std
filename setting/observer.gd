f95a1f9bec65144014b921c27ad752d2215c2c8c
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-11-20 17:40:02 -0800

    fix(setting): make `StdSettingsObserver` a node; remove observers from `StdSettingsRepository`

--- setting/observer.gd
+++ setting/observer.gd
@@ -6,35 +6,21 @@
 ##
 
 class_name StdSettingsObserver
-extends Resource
+extends Node
 
 # -- DEPENDENCIES -------------------------------------------------------------------- #
 
 const Config := preload("../config/config.gd")
-
-# -- DEFINITION ---------------------------------------------------------------------- #
-
-
-## `PropertyCallable` is a simple `StdSettingsObserver` which defers to the provided
-## callable. The callable's signature must match
-## `StdSettingsObserver.handle_value_change`.
-class PropertyCallable:
-	extends StdSettingsObserver
-
-	var callable: Callable
-
-	func _handle_value_change(
-		_config: Config, property: StdSettingsProperty, value: Variant
-	) -> void:
-		assert(callable is Callable, "invalid config: missing callable")
-		callable.call(property, value)
-
 
 # -- CONFIGURATION ------------------------------------------------------------------- #
 
 ## should_call_on_value_loaded controls whether this observer will be called when one of
 ## the matching properties is first loaded from disk.
 @export var should_call_on_value_loaded: bool = true
+
+# -- INITIALIZATION ------------------------------------------------------------------ #
+
+var _connected: Dictionary = {}
 
 # -- PUBLIC METHODS ------------------------------------------------------------------ #
 
@@ -47,17 +33,39 @@ func get_settings_properties() -> Array[StdSettingsProperty]:
 
 ## handle_value_change is called when a property that this observer is registered for
 ## has changed.
-func handle_value_change(
-	config: Config, property: StdSettingsProperty, value: Variant
-) -> void:
-	return _handle_value_change(config, property, value)
+func handle_value_change(property: StdSettingsProperty, value: Variant) -> void:
+	return _handle_value_change(property, value)
 
 
-## mount_observer_node creates a 'Node' which should be added to the scene tree for the
-## purpose of providing a hook to the observer. If the returned node is null, then no
-## node will be added.
-func mount_observer_node() -> Node:
-	return _mount_observer_node()
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
+
+
+func _ready() -> void:
+	assert(not _connected, "found dangling callbacks")
+
+	var properties := _get_settings_properties()
+
+	for p in properties:
+		var fn := func(v): _handle_value_change(p, v)
+
+		var err := p.value_changed.connect(fn)
+		assert(err == OK, "failed to connect to signal")
+
+		_connected[p] = fn
+
+	if should_call_on_value_loaded:
+		for p in properties:
+			_handle_value_change(p, p.get_value())
+
+
+func _exit_tree() -> void:
+	for p in _connected:
+		var fn: Callable = _connected[p]
+
+		if p.value_changed.is_connected(fn):
+			p.value_changed.disconnect(fn)
+
+	_connected.clear()
 
 
 # -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
@@ -68,11 +76,5 @@ func _get_settings_properties() -> Array[StdSettingsProperty]:
 	return []
 
 
-func _handle_value_change(
-	_config: Config, _property: StdSettingsProperty, _value
-) -> void:
+func _handle_value_change(_property: StdSettingsProperty, _value) -> void:
 	assert(false, "unimplemented")
-
-
-func _mount_observer_node() -> Node:
-	return null
