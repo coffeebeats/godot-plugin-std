31188d36a60c1f93aded1690814291994bc42e37
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-12-02 09:46:22 -0800

    feat(input): create placeholders for Godot-backed `InputDevice` components

---
+++ input/godot/device_bindings.gd
@@ -0,0 +1,56 @@
+##
+## std/input/godot/device_bindings.gd
+##
+## An implemention of `InputDevice.Bindings` which uses Godot's built-in action map
+## (plus custom action set handling) to manage available actions.
+##
+## TODO: Implement this class.
+##
+
+extends InputDevice.Bindings
+
+# -- SIGNALS ------------------------------------------------------------------------- #
+
+# -- DEPENDENCIES -------------------------------------------------------------------- #
+
+# -- DEFINITIONS --------------------------------------------------------------------- #
+
+# -- CONFIGURATION ------------------------------------------------------------------- #
+
+# -- INITIALIZATION ------------------------------------------------------------------ #
+
+# -- PUBLIC METHODS ------------------------------------------------------------------ #
+
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
+
+#func _draw() -> void:
+#	pass
+
+#func _enter_tree() -> void:
+#	pass
+
+#func _exit_tree() -> void:
+#	pass
+
+#func _notification(what) -> void:
+#	pass
+
+#func _physics_process(delta: float) -> void:
+#	pass
+
+#func _process(delta: float) -> void:
+#	pass
+
+#func _ready() -> void:
+#	pass
+
+#func _unhandled_input(event: InputEvent) -> void:
+#	pass
+
+# -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+# -- PRIVATE METHODS ----------------------------------------------------------------- #
+
+# -- SIGNAL HANDLERS ----------------------------------------------------------------- #
+
+# -- SETTERS/GETTERS ----------------------------------------------------------------- #
---
+++ input/godot/device_glyphs.gd
@@ -0,0 +1,56 @@
+##
+## std/input/godot/device_glyphs.gd
+##
+## An implemention of `InputDevice.Glyphs` which maps joypad names and origins to custom
+## glyphs using Godot's built-in `Input` class/SDL controller database.
+##
+## TODO: Implement this class.
+##
+
+extends InputDevice.Glyphs
+
+# -- SIGNALS ------------------------------------------------------------------------- #
+
+# -- DEPENDENCIES -------------------------------------------------------------------- #
+
+# -- DEFINITIONS --------------------------------------------------------------------- #
+
+# -- CONFIGURATION ------------------------------------------------------------------- #
+
+# -- INITIALIZATION ------------------------------------------------------------------ #
+
+# -- PUBLIC METHODS ------------------------------------------------------------------ #
+
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
+
+#func _draw() -> void:
+#	pass
+
+#func _enter_tree() -> void:
+#	pass
+
+#func _exit_tree() -> void:
+#	pass
+
+#func _notification(what) -> void:
+#	pass
+
+#func _physics_process(delta: float) -> void:
+#	pass
+
+#func _process(delta: float) -> void:
+#	pass
+
+#func _ready() -> void:
+#	pass
+
+#func _unhandled_input(event: InputEvent) -> void:
+#	pass
+
+# -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+# -- PRIVATE METHODS ----------------------------------------------------------------- #
+
+# -- SIGNAL HANDLERS ----------------------------------------------------------------- #
+
+# -- SETTERS/GETTERS ----------------------------------------------------------------- #
