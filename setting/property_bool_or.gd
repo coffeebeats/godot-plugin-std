f65256ee421d5e52c85025c268990d255e5fa225
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-12-10 13:59:43 -0800

    feat(setting): create "and" and "or" boolean logic settings properties

---
+++ setting/property_bool_or.gd
@@ -0,0 +1,33 @@
+##
+## std/setting/property_bool_or.gd
+##
+## `StdSettingsPropertyBoolOr` is a handle that identifies settings property that
+## evaluates to `true` if any of the configured boolean property values are `true`.
+##
+## NOTE: This settings property does *not* emit `value_changed` signals when its
+## dependent properties change. Instead, this setting should be manually checked as
+## needed.
+##
+
+class_name StdSettingsPropertyBoolOr
+extends StdSettingsPropertyBool
+
+# -- CONFIGURATION ------------------------------------------------------------------- #
+
+## properties is a list of boolean settings properties which will be evaluated. If any
+## are `true`, then this settings property evaluates to `true`.
+@export var properties: Array[StdSettingsPropertyBool] = []
+
+# -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+func _can_modify() -> bool:
+	return false
+
+func _get_value_from_config(_config: Config) -> Variant:
+	assert(not default, "invalid config; conflicting default value")
+
+	for property in properties:
+		if property.get_value():
+			return true
+
+	return false
