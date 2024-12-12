677d0eb5f6b5edda959dd9850da6dfcfc721f5b2
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-12-12 12:21:18 -0800

    fix(file): correctly form absolute path for exported projects (#118)

--- file/path.gd
+++ file/path.gd
@@ -15,13 +15,16 @@ extends Object
 ## make_project_path_absolute converts a project path into an absolute path, regardless
 ## of whether this script is running in the editor or an exported project.
 static func make_project_path_absolute(path: String) -> String:
-	assert(path.begins_with("res://") or path.begins_with("user://"), "invalid path")
-
-	if OS.has_feature("editor"):
+	if (
+		path.begins_with("user://")
+		or (path.begins_with("res://") and OS.has_feature("editor"))
+	):
 		return ProjectSettings.globalize_path(path)
 
 	path = path.trim_prefix("res://")
-	path = path.trim_prefix("user://")
+
+	if path.is_absolute_path():
+		return path
 
 	return OS.get_executable_path().get_base_dir().path_join(path)
 
