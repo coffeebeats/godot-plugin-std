226449d401d1c0abbb5840e7a7d3eca12676b585
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-12-19 13:00:31 -0800

    feat(input): implement Godot- and Steam-backed origin labels

--- input/godot/device_glyphs.gd
+++ input/godot/device_glyphs.gd
@@ -40,3 +40,39 @@ func _get_action_glyph(
 				return data
 
 	return null
+
+
+func _get_action_origin_label(
+	_device: int,
+	device_type: DeviceType,
+	_action_set: StringName,
+	action: StringName,
+) -> String:
+	assert(
+		device_type != StdInputDevice.DEVICE_TYPE_UNKNOWN,
+		"invalid argument: unknown device type",
+	)
+
+	# There's little point supporting origin labels for joypad devices - a glyph icon
+	# should be displayed instead.
+	if device_type != StdInputDevice.DEVICE_TYPE_KEYBOARD:
+		return ""
+
+	for event in InputMap.action_get_events(action):
+		if not event is InputEventKey:
+			continue
+
+		if (
+			event.keycode != KEY_NONE
+			and event.physical_keycode != KEY_NONE
+			and event.keycode != event.physical_keycode
+		):
+			assert(false, "invalid input; found conflicting keycodes")
+			continue
+
+		# NOTE: Only one of these properties can be set, so take the union of them in order
+		# to handle all of them.
+		var keycode: Key = event.keycode | event.physical_keycode
+		return OS.get_keycode_string(keycode).to_upper()
+
+	return ""
--- input/godot/glyph_set_keyboard.gd
+++ input/godot/glyph_set_keyboard.gd
@@ -10,13 +10,6 @@ extends StdInputGlyphSet
 
 # -- CONFIGURATION ------------------------------------------------------------------- #
 
-## include_label defines whether an origin label is included in the returned glyph data.
-@export var include_label: bool = false
-
-## capitalize_label controls whether the returned origin label is capitalized. Only used
-## if `include_label` is `true`.
-@export var capitalize_label: bool = true
-
 @export_subgroup("Command")
 
 @export var cmd_escape: Texture2D = null
@@ -325,16 +318,10 @@ func _get_origin_glyph(
 		KEY_EQUAL:
 			texture = symbol_equal
 
-	if not texture and not include_label:
+	if not texture:
 		return null
 
 	var data := GlyphData.new()
 	data.texture = texture
-
-	if include_label:
-		data.label = OS.get_keycode_string(keycode)
-
-		if capitalize_label:
-			data.label = data.label.to_upper()
 
 	return data
