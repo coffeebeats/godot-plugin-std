72d90e4d2928a7aa2b6485ed48f50d8805d95c46
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-01-21 08:21:58 -0800

    feat(config): implement a `reset()` method for `StdConfigSchema`

--- config/schema/schema.gd
+++ config/schema/schema.gd
@@ -23,6 +23,21 @@ const PROPERTY_USAGE_SERDE := PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_ST
 # -- PUBLIC METHODS ------------------------------------------------------------------ #
 
 
+## reset sets all serialization-enabled properties back to their default values.
+func reset() -> void:
+	for property in get_property_list():
+		if property[PROPERTY_KEY_USAGE] & PROPERTY_USAGE_SERDE != PROPERTY_USAGE_SERDE:
+			continue
+
+		var name: StringName = property[PROPERTY_KEY_NAME]
+		var value: Variant = get(name)
+
+		if not value is StdConfigItem:
+			continue
+
+		value.reset()
+
+
 ## store populates the provided `Config` instance with this schema's items.
 ##
 ## NOTE: Only exported, non-null `StdConfigItem` properties will be set on the `Config`.
