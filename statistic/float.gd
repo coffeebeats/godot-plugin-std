f635c968aa596501c8ebfe25cc74f1038478af90
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-01-30 20:30:44 -0800

    feat(statistic): create base types for stats

---
+++ statistic/float.gd
@@ -0,0 +1,19 @@
+##
+## std/statistic/float.gd
+##
+## StdStatValueFloat is a float user statistic.
+##
+
+class_name StdStatValueFloat
+extends StdStatValue
+
+# -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+
+func _get_value() -> float:
+	assert(false, "unimplemented")
+	return 0.0
+
+
+func _set_value(_value: float) -> void:
+	assert(false, "unimplemented")
---
+++ statistic/int.gd
@@ -0,0 +1,19 @@
+##
+## std/statistic/int.gd
+##
+## StdStatValueInt is an integer user statistic.
+##
+
+class_name StdStatValueInt
+extends StdStatValue
+
+# -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+
+func _get_value() -> int:
+	assert(false, "unimplemented")
+	return 0
+
+
+func _set_value(_value: int) -> void:
+	assert(false, "unimplemented")
