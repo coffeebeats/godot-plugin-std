6d81d849161b0a7c96afa7100d8b4287df6f87ef
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-02-09 13:10:03 -0800

    feat(sound): create resources to manage static and dynamic audio buses

---
+++ sound/bus.gd
@@ -0,0 +1,41 @@
+##
+## std/sound/bus.gd
+##
+## StdAudioBus describes an audio bus.
+##
+
+class_name StdAudioBus
+extends Resource
+
+# -- PUBLIC METHODS ------------------------------------------------------------------ #
+
+
+## get_bus_index returns the index of the audio bus within the audio server.
+func get_bus_index() -> int:
+	return AudioServer.get_bus_index(_get_bus_name())
+
+
+## get_bus_name returns the name of the audio bus.
+func get_bus_name() -> StringName:
+	return _get_bus_name()
+
+
+## setup performs any necessary work to create the specified audio bus.
+func setup() -> void:
+	_setup()
+
+
+# -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+
+func _get_bus_name() -> StringName:
+	assert(false, "unimplemented")
+	return &""
+
+
+func _setup() -> void:
+	pass
+
+
+func _teardown() -> void:
+	pass
