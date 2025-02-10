d86c9ce199971bf6d77ee29ca476f78a143ac363
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-02-10 09:04:40 -0800

    fix(sound): don't make `StdAudioBusDynamic` modifiable at runtime

--- sound/bus_dynamic.gd
+++ sound/bus_dynamic.gd
@@ -4,6 +4,11 @@
 ## StdAudioBus describes a dynamic audio bus which can be created on-the-fly for game-
 ## specific purposes.
 ##
+## NOTE: This class is *not* meant to be modified during audio playback. Modulations
+## of an audio bus should be driven through an `StdSoundParam` rather than on the audio
+## bus resource directly. As such, changes to exported properties will not be handled
+## for existing audio buses.
+##
 
 class_name StdAudioBusDynamic
 extends StdAudioBus
@@ -11,18 +16,17 @@ extends StdAudioBus
 # -- CONFIGURATION ------------------------------------------------------------------- #
 
 ## send_bus is the audio bus to which this dynamic bus will be routed to.
-@export var send_bus: StdAudioBus = null:
-	set = _set_send_bus
+@export var send_bus: StdAudioBus = null
 
 ## effects is a list of audio effects that will be applied to the created audio bus.
-@export var effects: Array[AudioEffect] = []:
-	set = _set_bus_effects
+@export var effects: Array[AudioEffect] = []
 
 # -- INITIALIZATION ------------------------------------------------------------------ #
 
 ## NOTE: The bus name needs to be stored because this object won't be valid during the
 ## "predelete" phase; see https://github.com/godotengine/godot/issues/6784.
 var _bus_name: StringName = &""
+var _is_setup_complete: bool = false
 
 # -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
 
@@ -30,7 +34,7 @@ var _bus_name: StringName = &""
 func _notification(what: int) -> void:
 	match what:
 		NOTIFICATION_PREDELETE:
-			if not _bus_name:
+			if not _is_setup_complete:
 				return  # Never used, so no need to delete.
 
 			# NOTE: Can't use `_get_bus_name()`; see note on `_bus_name`.
@@ -39,6 +43,8 @@ func _notification(what: int) -> void:
 				return
 
 			AudioServer.remove_bus(index)
+
+			_logger.debug("Removed audio bus.", {&"name": _bus_name, &"index": index})
 
 
 # -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
@@ -49,32 +55,47 @@ func _get_bus_name() -> StringName:
 
 
 func _setup() -> void:
-	_bus_name = _get_bus_name()
+	assert(send_bus is StdAudioBus, "invalid config; missing output audio bus")
+	assert(effects is Array[AudioEffect], "invalid config; wrong type")
+
+	if _is_setup_complete:
+		assert(
+			AudioServer.get_bus_index(_get_bus_name()) > -1,
+			"invalid state; missing bus",
+		)
+
+		return  # Already set up; no need to do anything.
+
+	var bus_name := _get_bus_name()
 
 	assert(
-		AudioServer.get_bus_index(_bus_name) == -1,
+		AudioServer.get_bus_index(bus_name) == -1,
 		"invalid state; found existing bus",
 	)
 
 	var index: int = AudioServer.bus_count
 	AudioServer.add_bus(index)
-	AudioServer.set_bus_name(index, _bus_name)
+	AudioServer.set_bus_name(index, bus_name)
 
-	# Trigger property setters.
-	send_bus = send_bus
-	effects = effects
+	_update_audio_bus_send_bus(index)
+	_update_audio_bus_effects(index)
+
+	_logger.debug("Added audio bus.", {&"name": bus_name, &"index": index})
+
+	_bus_name = bus_name
+	_is_setup_complete = true
 
 
-# -- SETTERS/GETTERS ----------------------------------------------------------------- #
+# -- PRIVATE METHODS ----------------------------------------------------------------- #
 
 
-func _set_bus_effects(value: Array[AudioEffect]) -> void:
-	assert(value is Array[AudioEffect], "invalid config; wrong type")
-	effects = value
+func _update_audio_bus_send_bus(bus_index: int) -> void:
+	var send_bus_name := send_bus._get_bus_name()
+	assert(AudioServer.get_bus_index(send_bus_name) > -1, "invalid state; missing bus")
+	AudioServer.set_bus_send(bus_index, send_bus_name)
 
-	var bus_index := AudioServer.get_bus_index(_get_bus_name())
-	assert(bus_index > -1, "invalid state; missing bus index")
 
+func _update_audio_bus_effects(bus_index: int) -> void:
 	# Clear any existing bus effects. There shouldn't be any because effects should
 	# not be changed at runtime (i.e. this runs just once).
 	for effect_index in AudioServer.get_bus_effect_count(bus_index):
@@ -87,19 +108,3 @@ func _set_bus_effects(value: Array[AudioEffect]) -> void:
 			continue
 
 		AudioServer.add_bus_effect(bus_index, effect)
-
-	changed.emit()
-
-
-func _set_send_bus(value: StdAudioBus) -> void:
-	assert(value is StdAudioBus, "invalid config; wrong type")
-	send_bus = value
-
-	var bus_index := AudioServer.get_bus_index(_get_bus_name())
-	assert(bus_index > -1, "invalid state; missing bus index")
-
-	var send_bus_name := value._get_bus_name()
-	assert(AudioServer.get_bus_index(send_bus_name) > -1, "invalid state; missing bus")
-	AudioServer.set_bus_send(bus_index, send_bus_name)
-
-	changed.emit()
