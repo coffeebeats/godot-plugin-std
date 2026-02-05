f0b6f0de81e630313578015f98eb9aee747ce9ce
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2026-02-04 20:59:11 -0800

    chore(router): scaffold the route container

---
+++ router/container.gd
@@ -0,0 +1,43 @@
+##
+## router/container.gd
+##
+## StdRouteContainer is a placeholder node that can be swapped for nested route content
+## (e.g. nested route views or modals). When a nested route is navigated to, its scene
+## is instantiated and added as a child of the matching `StdRouteContainer` node in the
+## parent route's rendered scene.
+##
+## Note that when a route is navigated to, one and only one route container must match.
+## This means that a route container for the router's game root, the router's modal
+## root, and any nested routes, all must exist. Furthermore, only one of each type may
+## exist under the parent route's portion of the scene tree.
+##
+
+class_name StdRouteContainer
+extends Node
+
+# -- DEFINITIONS --------------------------------------------------------------------- #
+
+## ViewType is the type of route view this placeholder accepts. This affects how the
+## rendered scene will be displayed.
+enum ViewType {
+	VIEW_TYPE_CONTENT,  ## Standard, router-managed game scene.
+	VIEW_TYPE_MODAL,  ## Router-managed game scene that overlays the standard content.
+}
+
+# -- CONFIGURATION ------------------------------------------------------------------- #
+
+## view_type is the type of nested route content this view accepts. Content views are
+## for regular nested routes, while modal views are for nested modal overlays.
+@export var view_type: ViewType = ViewType.VIEW_TYPE_CONTENT
+
+# -- ENGINE METHODS (OVERRIDES) -------------------------------------------------- #
+
+
+func _enter_tree() -> void:
+	# FIXME: Figure out how to locate the router/register self.
+	assert(false)
+
+
+func _exit_tree() -> void:
+	# FIXME: Figure out how to locate the router/unregister self.
+	assert(false)
