8ce1a3f4dc9ff31346109d8196bdbbcc157b3365
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2026-02-05 09:24:03 -0800

    fix(router): register containers as they enter the scene tree

--- router/container.gd
+++ router/container.gd
@@ -17,9 +17,12 @@ extends Node
 
 # -- DEFINITIONS --------------------------------------------------------------------- #
 
+## GROUP_ROUTE_CONTAINER is the StdGroup ID for route container registration.
+const GROUP_ROUTE_CONTAINER := &"std/router:container"
+
 ## ViewType is the type of route view this placeholder accepts. This affects how the
 ## rendered scene will be displayed.
-enum ViewType {
+enum ViewType {  # gdlint:ignore=class-definitions-order
 	VIEW_TYPE_CONTENT,  ## Standard, router-managed game scene.
 	VIEW_TYPE_MODAL,  ## Router-managed game scene that overlays the standard content.
 }
@@ -30,14 +33,32 @@ enum ViewType {
 ## for regular nested routes, while modal views are for nested modal overlays.
 @export var view_type: ViewType = ViewType.VIEW_TYPE_CONTENT
 
-# -- ENGINE METHODS (OVERRIDES) -------------------------------------------------- #
+# -- INITIALIZATION ------------------------------------------------------------------ #
+
+## route is the route this container is currently assigned to. Set by the router when
+## the container is discovered during a navigation transition.
+var route: StdRoute = null
+
+# -- PUBLIC METHODS ------------------------------------------------------------------ #
+
+
+## all returns all registered StdRouteContainer instances.
+static func all() -> Array[StdRouteContainer]:
+	var result: Array[StdRouteContainer] = []
+
+	for member in StdGroup.with_id(GROUP_ROUTE_CONTAINER).list_members():
+		if member is StdRouteContainer:
+			result.append(member)
+
+	return result
+
+
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
 
 
 func _enter_tree() -> void:
-	# FIXME: Figure out how to locate the router/register self.
-	assert(false)
+	StdGroup.with_id(GROUP_ROUTE_CONTAINER).add_member(self)
 
 
 func _exit_tree() -> void:
-	# FIXME: Figure out how to locate the router/unregister self.
-	assert(false)
+	StdGroup.with_id(GROUP_ROUTE_CONTAINER).remove_member(self)
