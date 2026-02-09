85528ac71279800d68467387a281ce7b0138f559
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2026-02-08 21:23:35 -0800

    feat(config): add metadata section for schemas to store their version

---
+++ config/schema/meta.gd
@@ -0,0 +1,18 @@
+##
+## std/config/schema/meta.gd
+##
+## StdConfigSchemaMeta is an internal config item that stores schema version metadata.
+##
+
+class_name StdConfigSchemaMeta
+extends StdConfigItem
+
+# -- CONFIGURATION ------------------------------------------------------------------- #
+
+@export var version: int = 0
+
+# -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+
+func _get_category() -> StringName:
+	return &"__meta__"
